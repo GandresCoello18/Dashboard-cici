307e7fe295d9b4d51ee1545387bb68e1692926b8
GandresCoello18 <goyeselcoca@gmail.com>
2021-04-03 11:16:43 -0500

    change background pie recomendation

--- src/components/Products/Details/staticsRecomendado.tsx
+++ src/components/Products/Details/staticsRecomendado.tsx
@@ -21,9 +21,9 @@ const Recomendado = ({ Recommendation }: Props) => {
         data: Recommendation?.length ? Recommendation.map(item => item.total) : [],
         label: 'Recomendado',
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
         ],
       },
     ],
